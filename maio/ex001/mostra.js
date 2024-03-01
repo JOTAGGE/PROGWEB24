69c9f77b0cf78c33b545f2cdc6215846a48e3e3a
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2024-03-01 23:01:22 +0000

    soma travado

---
+++ maio/ex001/mostra.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var x1 = document.getElementById("myText1").value;
+    document.getElementById("demo1").innerHTML = x1;
+    var x2 = document.getElementById("myText2").value;
+    document.getElementById("demo2").innerHTML = x2;
+    var x3 = document.getElementById("myText3").value;
+    document.getElementById("demo3").innerHTML = x3;
+  }
---
+++ maio/ex001/soma.js
@@ -0,0 +1,6 @@
+function soma(){
+    var n1 = document.getElementById("n1").value;
+    var n2 = document.getElementById("n2").value;
+    var n3 = document.getElementById("n3").value;
+    document.getElementById("soma").innerHTML = n1 + n2 + n3;
+}
