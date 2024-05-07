beb14564ec331cd281271067dcafe55d46ed7e89
JOTAGGE <108813623+JOTAGGE@users.noreply.github.com>
2024-05-07 23:38:39 +0000

    salvando o código

--- Atividade2/IMC.js
+++ Atividade2/IMC.js
@@ -1,46 +1,29 @@
 const prompt = require("prompt-sync")();
 // Solicitar peso e altura do usuário
-let peso = prompt("Digite seu peso em kg:");
-let altura = prompt("Digite sua altura em metros:");
-
-// Converter entrada para números
-peso = parseFloat(peso);
-altura = parseFloat(altura);
-
-peso = document.getElementById('peso');
-altura = document.getElementById('altura');
+let peso = parseFloat(prompt("Digite seu peso em kg:"));
+let altura = parseFloat(prompt("Digite sua altura em metros:"));
 
 // Calcular IMC
-let imc = peso / (altura * altura);
+const imc = peso / (altura * altura);
 
-function mostrarResultados() {
-
-    imc = document.getElementById('imc');
-
-    imc.mostrarResultados = imc;
-
-    let categoria = document.getElementById('categoria');
+// Exibir categoria de IMC
+if (imc < 18.5) {
+    console.log("Abaixo do peso");
+        
+} else if (imc >= 18.5 && imc <= 24.9) {
+    console.log("Peso normal");
     
-
-    // Exibir categoria de IMC
-    if (imc < 18.5) {
-        console.log("Abaixo do peso");
-        categoria.mostrarResultados = 'Abaixo do peso';    
-    } else if (imc >= 18.5 && imc <= 24.9) {
-        console.log("Peso normal");
-        categoria.mostrarResultados = 'Peso Normal';
-    } else if (imc >= 25.0 && imc <= 29.9) {
-        console.log("Excesso de peso");
-        categoria.mostrarResultados = 'Abaixo do Excesso de Peso';
-    } else if (imc >= 30.0 && imc <= 34.9) {
-        console.log("Obesidade grau I");
-        categoria.mostrarResultados = 'Obesidade grau I';
-    } else if (imc >= 35.0 && imc <= 39.9) {
-        console.log("Obesidade grau II");
-        categoria.mostrarResultados = 'Obesidade Grau II';
-    } else {
-        console.log("Obesidade grau III");
-        categoria.mostrarResultados = 'Obesidade Grau III';        
-    }    
-}
+} else if (imc >= 25.0 && imc <= 29.9) {
+    console.log("Excesso de peso");
+    
+} else if (imc >= 30.0 && imc <= 34.9) {
+    console.log("Obesidade grau I");
+    
+} else if (imc >= 35.0 && imc <= 39.9) {
+    console.log("Obesidade grau II");
+    
+} else {
+    console.log("Obesidade grau III");
+           
+}    
 
